e5c9f9cfebfc61a4145595321de3dc5b872ddedf
delightful-dispatch-9896 <maychen2580@hotmail.com>
2020-07-27 02:01:54 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,27 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
-  puts "   |    |"
-  puts "-------------"
-  puts "   |    |"
-  puts "-------------"
-  puts "   |    |"
+puts "Welcome to Tic Tac Toe"
+
+def display_board
+puts "   |    |"
+puts "--------------"
+puts "   |    |"
+puts "--------------"
+puts "   |    |"
+end
+  
+puts "   |    |   "
+
+puts "    |    |  "
+puts " --------------"
+puts "    |    |  "
+puts " --------------"
+puts "    |    |   "
+
+puts"-----------"
+
+print "   |   |   "
+print "-----------"
+print "   |   |   "
+print " ----------"
+print "    |    |   "
+
